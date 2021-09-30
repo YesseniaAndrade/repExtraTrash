4ccb9ac9b5d9af1f42a73cb60bc86547222458b2
Yessenia <l19940190@tlajomulco.tecnm.mx>
2021-09-29 20:14:29 -0500

    Esta es la primer versión

---
+++ Asterisco.py
@@ -0,0 +1,10 @@
+class Asterisco():
+    def ast(self):
+        s=0
+        c=""
+        for x in range(1,11):
+            c=c+"*"
+            print(x,' ',c)
+               
+asr=Asterisco()
+asr.ast()
